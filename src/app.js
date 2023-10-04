8bc1e2cb4c5c3a834715b60b56477f425b8cc421
Stefanie M <stefanielynn9008@gmail.com>
2023-10-04 14:32:05 -0700

    Took out the console log

--- src/app.js
+++ src/app.js
@@ -53,7 +53,6 @@ function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
-  console.log(cityInputElement.value);
 }
 
 search("London");
