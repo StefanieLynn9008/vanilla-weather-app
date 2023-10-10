a6e646dfe1834a758a879941ebfd050641f919cf
Stefanie M <stefanielynn9008@gmail.com>
2023-10-10 13:48:56 -0700

    Updated CSS styling, added some classes in HTML

--- src/app.js
+++ src/app.js
@@ -32,7 +32,6 @@ function formatDay(timestamp) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
   let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
